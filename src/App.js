5157bb6e7dc542acc79352208a9e3f0d61065034
Anudeepthi <anudeepthikolagani.999@gmail.com>
2026-03-26 15:00:28 +0530

    Implemented Upload component to upload the files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
+import Upload from "./components/Upload";
 
 function App() {
-  return <div className="App">HIIIIIIIIII</div>;
+  return <div className="App">
+    <Upload/>
+  </div>;
 }
 
 export default App;
