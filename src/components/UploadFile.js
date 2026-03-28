036d3b9453d4b222d78802970d3ac6eaef149d7f
Anudeepthi <anudeepthikolagani.999@gmail.com>
2026-03-28 22:55:48 +0530

    Changes related to track the upload progress of the uploaded file

--- src/components/UploadFile.js
+++ src/components/UploadFile.js
@@ -1,4 +1,4 @@
-import { CloudUpload } from "lucide-react";
+import { Check, CloudUpload, File, X } from "lucide-react";
 import Logo from "./Logo";
 import { useEffect, useRef, useState } from "react";
 import axios from "axios";
@@ -7,6 +7,7 @@ const UploadFile = () => {
   const inputRef = useRef(null);
   const [selectedFiles, setSelectedFiles] = useState([]);
   const [inValidFiles, setInValidFiles] = useState([{}]);
+  const [progress, setProgress] = useState(0);
 
   const MAX_SIZE = 2 * 1024 * 1024;
   const apiURL = process.env.REACT_APP_API_URL;
@@ -29,6 +30,12 @@ const UploadFile = () => {
     const res = await axios.post(`${apiURL}/upload`, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
+      },
+      onUploadProgress: (ProgressEvent) => {
+        const percentComplated = Math.round(
+          (ProgressEvent.loaded * 100) / ProgressEvent.total,
+        );
+        setProgress(percentComplated);
       },
     });
   };
@@ -75,43 +82,74 @@ const UploadFile = () => {
   };
 
   return (
-    <div className="px-4 py-2 ">
+    <div className="px-4 md:px-6 py-2 min-h-screen">
       <Logo />
 
-      <div className="flex flex-col gap-4 mx-auto w-[40vw] mt-10">
+      <div className="flex flex-col gap-4 mx-auto w-full md:w-[90vw] lg:w-[40vw] max-w-2xl mt-10">
         {/* Title */}
-        <h2 className="text-3xl font-bold">Welcome to Doc Assistant</h2>
-        <p className="text-gray-700 text-sm">
+        <h2 className="text-2xl md:text-3xl font-bold text-center ">
+          Welcome to Doc Assistant
+        </h2>
+        <p className="text-gray-700 text-xs md:text-sm text-center ">
           Upload you documents and let Doc Assigstant uncover key insigts and
           answer your questions
         </p>
 
         {/* Upload or drag and drop files */}
-        <div className="border border-gray-800 rounded-xl flex flex-col gap-5 justify-center p-5">
-          <CloudUpload className="w-20 h-20 flex mx-auto" />
-          <div>Drag & drop your files</div>
-          <p>OR</p>
+        <div className="border border-gray-800 rounded-xl flex flex-col gap-5 justify-center p-5 md:p-8">
+          <CloudUpload
+            className="w-16 md:w-20 h-16 md:h-20 flex mx-auto"
+            strokeWidth={1}
+          />
+          <div className="text-center text-sm md:text-base">
+            Drag & drop your files
+          </div>
+          <p className="text-center">OR</p>
           <input
             ref={inputRef}
             type="file"
             className="hidden"
-            multiple
+            // multiple
             accept=".pdf, .txt, .doc, .docs"
             onChange={handleFileChange}
           />
           <button
-            className="w-[10vw]  mx-auto bg-gray-900 text-white border rounded-lg px-4 py-2"
+            className="w-full md:w-auto mx-auto bg-gray-900 text-white border rounded-lg px-6 py-2 text-sm md:text-base"
             onClick={handleBrowseFiles}
           >
             Browse files
           </button>
 
-          <p>PDF, Wordpad or Text, Max 100 MB each.</p>
+          <p className="text-xs md:text-sm text-center">
+            PDF, Wordpad or Text, Max 100 MB each.
+          </p>
         </div>
 
         {/* Track the prgress of files */}
         {selectedFiles.length > 0 && (
-          <div className="flex justify-start">Uploading files</div>
+          <div className="flex flex-col gap-2">
+            <h2 className="text-sm font-bold mt-2">Uploading files</h2>
+            <div className="flex gap-2 mt-2 items-center">
+              <File className="w-4 h-4" />
+              <div className="flex flex-col items-center justify-center"></div>
+              <div className="w-full h-1.5 rounded-lg bg-gray-300">
+                <div
+                  className="w-full h-full bg-black"
+                  style={{ width: `${progress}%` }}
+                ></div>
+                <div className="text-sm text-black text-center">
+                  {progress}%
+                </div>
+              </div>
+              <div>
+                {progress < 100 ? (
+                  <X className="w-5 h-5 text-red-500 text-bold" />
+                ) : (
+                  <Check className="w-5 h-5 text-green-500 text-bold" />
+                )}
+              </div>
+            </div>
+          </div>
         )}
         {/* PENDING: PROGRESS TRACKING AND CANCELLING THE UPLOAD */}
         <div className="text-red-500 font-semibold">
